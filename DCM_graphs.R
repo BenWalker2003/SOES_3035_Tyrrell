6d65f41f00535d2622f5d7227d794c53afcdc9ae
holly-sher1 <hs7g22@soton.ac.uk>
2025-03-10 12:08:06 +0000

    Updated_DCM_Graphs.R

--- DCM_graphs.R
+++ DCM_graphs.R
@@ -1,9 +1,10 @@
-
 setwd("~/Holly/University/Year 3/Oceanography Research Training/Group Project")
 library(tidyr)
 library(dplyr)
 library(ggplot2)
 load("BGC_argoPARfiltered_datecorrected.RData")
+
+#### data manipulation ####
 
 # filtering data so that only quality control 1 and 8 are included
 # for pressure, chlorophyll a and downwelling irradiance
@@ -18,32 +19,55 @@ argo3 <- argo2 %>%
   group_by(WMOID, CYCLE_NUMBER) %>%
   mutate(maxPAR = max(as.numeric(DOWNWELLING_PAR)))
 
-# selecting the row with maximum chlorophyll (DCM)
-dcm <- argo3 %>% 
+# add the median chlorophyll for depth <= 15m in each profile
+argo3 <- argo3 %>%
   group_by(WMOID, CYCLE_NUMBER) %>%
-  slice_max(CHLA)
+  mutate(med_CHLA_15 = median(CHLA[PRES <= 15], na.rm = TRUE)) %>%
+  ungroup()
 
-# exclude rows with max chl shallower than 10m depth, not a DCM
-# dcm <- dcm %>%
-#   filter(PRES > 10)
+# only keep profiles with at least 20 measurements
+argo4 <- argo3 %>%
+  group_by(WMOID, CYCLE_NUMBER) %>%
+  filter(n() >= 20) %>%
+  ungroup()
 
-# making some disaster graphs
+# select the row with maximum chlorophyll (DCM) from each profile
+dcm1 <- argo3 %>% 
+  group_by(WMOID, CYCLE_NUMBER) %>%
+  slice_max(CHLA, with_ties = FALSE)
 
-# surface PAR and depth of max chl
+# remove rows with NA values for median chlorophyll (no measurements <15m)
+dcm2 <- dcm1 %>% drop_na(med_CHLA_15)
+
+# remove rows where max CHLA is in the upper 15m
+dcm3 <- dcm2 %>%
+  filter(PRES > 15)
+
+# only keep rows where max CHLA is at least 2x larger than the upper 15m median
+dcm <- dcm3 %>%
+  filter(CHLA >= 2*(med_CHLA_15))
+
+
+#### graphs ####
+
+# surface PAR and depth of DCM
 g1 <- ggplot(data = dcm, aes(maxPAR, PRES, colour = CHLA)) + theme_bw() + geom_point() +
   scale_y_reverse()
 g1 + scale_colour_continuous(type = "viridis")
 
-# PAR at depth of max chl
+# PAR at depth of DCM
 g2 <- ggplot(data = dcm, aes(DOWNWELLING_PAR, PRES, colour = CHLA)) + theme_bw() +
   geom_point() + scale_y_reverse()
 g2 + scale_colour_continuous(type = "viridis")
 
-# chl against PAR at depth of max chl
-ggplot(data = dcm, aes(DOWNWELLING_PAR, CHLA)) + theme_bw() + geom_point()
+# chl against PAR at DCM
+ggplot(data = dcm, aes(DOWNWELLING_PAR, CHLA, colour = PRES)) + theme_bw() +
+  geom_point() + scale_colour_continuous(type = "viridis")
 
-# max chl against surface PAR
-ggplot(data = dcm, aes(maxPAR, CHLA)) + theme_bw() + geom_point()
+# DCM chl against surface PAR
+ggplot(data = dcm, aes(maxPAR, CHLA, colour = PRES)) + theme_bw() + geom_point() +
+  scale_colour_continuous(type = "viridis")
 
-# chl at depth of max chl
-ggplot(data = dcm, aes(CHLA, PRES)) + theme_bw() + geom_point()
+# chl and depth of DCM
+ggplot(data = dcm, aes(CHLA, PRES)) + theme_bw() + geom_point() + scale_y_reverse()
+
